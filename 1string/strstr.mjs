611f9515693235853f0d4230512cf9ea8e3d23e8
Joe Zhou <Xiaoyong.Zhou@prometheanworld.com>
2020-02-15 10:17:23 -0800

    need to think about loop end condition and how it ends

--- 1string/strstr.mjs
+++ 1string/strstr.mjs
@@ -1,29 +1,39 @@
 /*
-Description
-中文
-English
-For a given source string and a target string, you should output the first index(from 0) of target string in source string.
-
+For a given source string and a target string, you should output the first index(from 0) of 
+target string in source string.
 If target does not exist in source, just return -1.
-
 Have you met this question in a real interview?  
 Clarification
-Do I need to implement KMP Algorithm in a real interview?
-
-Not necessary. When you meet this problem in a real interview, the interviewer may just want to test your basic implementation ability. But make sure you confirm with the interviewer first.
 Example
 Example 1:
-
 Input: source = "source" ，target = "target"
 Output: -1	
 Explanation: If the source does not contain the target content, return - 1.
 Example 2:
-
 Input:source = "abcdabcdefg" ，target = "bcd"
 Output: 1	
 Explanation: If the source contains the target content, return the location where the target first appeared in the source.
 Challenge
 O(n2) is acceptable. Can you implement an O(n) algorithm? (hint: KMP)
-
-Related Problems
 */
+//pre: source.length - start >= target.length
+function match(source, start, target){
+   for(const c of target){
+       if(source[start] === c){
+           start++;
+       }
+       else {
+           return false;
+       }
+   } 
+   return true;
+}
+
+export function strstr(source, target){
+    if(!source || !target) return -1;
+    let start = 0;
+    while( source.length - start >= target.length){
+        if(match(source, start, target)) return start;
+    }
+    return -1;
+}
---
+++ 1string/strstr.test.mjs
@@ -0,0 +1,26 @@
+import test from 'ava';
+import {strict as assert} from 'assert';
+import {strstr} from "./strstr.mjs";
+
+/*
+Input: source = "source" ，target = "target"
+Output: -1	
+Explanation: If the source does not contain the target content, return - 1.
+Example 2:
+Input:source = "abcdabcdefg" ，target = "bcd"
+Output: 1	
+E
+*/
+
+test('source and target should return -1', t => {
+    let result = strstr('source', 'target');
+    console.log(`result is ${result}`);
+    assert.deepEqual(result, -1, "should return ");
+});
+
+test('abcdabcdefg and bcd should return 1', t => {
+    let result = strstr('abcdabcdefg', 'bcd');
+    console.log(`result is ${result}`);
+    assert.deepEqual(result, 1, "should return 1");
+});
+
