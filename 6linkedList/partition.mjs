c6a53bfc53f6018c22ff725cd6cc22ed34025624
Joe Zhou <Xiaoyong.Zhou@prometheanworld.com>
2020-03-14 10:48:12 -0700

    partition

--- 6linkedList/partition.mjs
+++ 6linkedList/partition.mjs
@@ -3,8 +3,26 @@
  * @param x: An integer
  * @return: A ListNode
  */
-const partition = function (head, x) {
-    // write your code here
+export function partition (head, x) {
+    if(head === null || head.next === null) return head;
+    let dummyLeft = new ListNode(0);
+    let dummyRight = new ListNode(0);
+    let leftPre = dummyLeft;
+    let rightPre = dummyRight;
+    while(head !== null){
+        if(head.val < x){
+            leftPre.next = head;
+            leftPre = leftPre.next;
+        }else 
+        {
+            rightPre.next = head;
+            rightPre = rightPre.next;
+        }
+        head = head.next;
+    }
+    leftPre.next = dummyRight.next;;
+    rightPre.next = null;
+    return dummyLeft.next;
 }
 
 
