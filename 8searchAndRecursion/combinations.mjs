58c8384a4978ea880654672735da513035eac7bf
Joe Zhou <Xiaoyong.Zhou@prometheanworld.com>
2020-03-17 21:05:50 -0700

    combine v1

---
+++ 8searchAndRecursion/combinations.mjs
@@ -0,0 +1,30 @@
+
+function combineHelper(n, k, currentSet, index, collector){
+    if(currentSet.length === k) {
+        collector.push([...currentSet]);
+        return;
+    }
+
+    if(index >= n.length) return;
+
+    currentSet.push(n[index]);
+    combineHelper(n, k, currentSet, index+1, collector);
+
+    currentSet.pop();
+    combineHelper(n, k, currentSet, index+1, collector);
+}
+/**
+ * @param n: Given the range of numbers
+ * @param k: Given the numbers of combinations
+ * @return: All the combinations of k numbers out of 1..n
+ */
+export function combine (n, k) {
+    let currentSet = [];
+    let collector = [];
+    let inputs = [];
+    for(let i = 1; i <= n; i++) inputs.push(i);
+    combineHelper(inputs, k, currentSet, 0, collector);
+    return collector;
+}
+
+
---
+++ 8searchAndRecursion/combinations.test.mjs
@@ -0,0 +1,11 @@
+import test from 'ava';
+import {strict as assert} from 'assert';
+import {combine} from "./combinations.mjs";
+
+test('4 should be [[1,2],[1,3],[1,4], [2,3], [2,4], [3,4]]', t => {
+    let result = combine(4, 2);
+    console.log(`result is ${result}`);
+    assert.deepEqual(result, [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]], "should return [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]");
+});
+
+
